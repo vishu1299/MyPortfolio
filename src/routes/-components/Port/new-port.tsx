4bb5d97f54b247d14f3e5e738d857e0148b88c85
vishu1299 <vishvasgupta129@gmail.com>
2024-10-16 19:12:41 +0530

    resume section updated

--- src/routes/-components/Port/new-port.tsx
+++ src/routes/-components/Port/new-port.tsx
@@ -5,38 +5,26 @@ import "../../../style.css";
 
 const Newfile = () => {
   // References for the left and right containers
-  const leftContainerRefs = [useRef(null), useRef(null)];
-  const rightContainerRefs = [useRef(null), useRef(null)];
+  const leftContainerRef1 = useRef(null);
+  const leftContainerRef2 = useRef(null);
+  const rightContainerRef1 = useRef(null);
+  const rightContainerRef2 = useRef(null);
 
   // In view states for each container
-  const leftInViews = [
-    useInView(leftContainerRefs[0], { once: true }),
-    useInView(leftContainerRefs[1], { once: true }),
-  ];
-  const rightInViews = [
-    useInView(rightContainerRefs[0], { once: true }),
-    useInView(rightContainerRefs[1], { once: true }),
-  ];
+  const leftInView1 = useInView(leftContainerRef1, { once: true });
+  const leftInView2 = useInView(leftContainerRef2, { once: true });
+  const rightInView1 = useInView(rightContainerRef1, { once: true });
+  const rightInView2 = useInView(rightContainerRef2, { once: true });
 
-  // Data for the timeline items
-  const timelineItems = [
-    {
-      title: "BCA",
-      date: "2020 - 2023",
-      description:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora sapiente quam, molestias nemo rem porro!",
-    },
-    {
-      title: "BCA",
-      date: "2020 - 2023",
-      description:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora sapiente quam, molestias nemo rem porro!",
-    },
-  ];
+  // Check screen size to adjust animations
+  const isMobile = window.innerWidth <= 640;
 
   return (
     <>
-      <div className=" mx-auto pt-10 px-4 md:px-8 lg:px-10 text-center flex flex-col gap-3 shrink-0 mt-40 md:mt-20 lg:mt-24 xl:mt-36 responsive-div">
+      <div
+        id="portfolio"
+        className="mx-auto sm:pb-16 pt-10 px-4 md:px-8 lg:px-10 text-center flex flex-col gap-3 shrink-0 mt-40 md:mt-20 lg:mt-24 xl:mt-36 responsive-div"
+      >
         <div className="flex gap-3 justify-center items-center">
           <img
             className="w-[1rem] my-auto"
@@ -64,53 +52,104 @@ const Newfile = () => {
       </div>
 
       <div className="timeline w-full sm:w-[100%] xl:w-[60%] relative mx-auto my-16">
-        {timelineItems.map((item, index) => (
-          <div key={index}>
-            <motion.div
-              ref={leftContainerRefs[index]}
-              initial={{ opacity: 0, x: -100 }}
-              animate={leftInViews[index] ? { opacity: 1, x: 0 } : {}}
-              transition={{ duration: 1, delay: index * 0.2 }}
-              className="containers left-container"
-            >
-              <img src="/record.png" alt="" />
-              <div className="text-box flex flex-col gap-1 sm:gap-3 sm:text-end shadow-lg rounded-lg p-4 hover:shadow-xl transition-all duration-300 transform hover:scale-105">
-                <h1 className="text-base sm:text-lg font-semibold text-white">
-                  {item.title}
-                </h1>
-                <small className="text-[#D21C23] font-semibold text-[10px] sm:text-xs">
-                  {item.date}
-                </small>
-                <p className="text-[12px] sm:text-sm text-gray-300">
-                  {item.description}
-                </p>
-                <div className="left-container-arrow"></div>
-              </div>
-            </motion.div>
-
-            <motion.div
-              ref={rightContainerRefs[index]}
-              initial={{ opacity: 0, x: 100 }}
-              animate={rightInViews[index] ? { opacity: 1, x: 0 } : {}}
-              transition={{ duration: 1, delay: index * 0.2 + 0.1 }}
-              className="containers right-container"
-            >
-              <img src="/record.png" alt="" />
-              <div className="text-box  flex flex-col gap-3  shadow-lg rounded-lg p-4 hover:shadow-xl transition-all duration-300 transform hover:scale-105">
-                <h1 className="text-base sm:text-lg font-semibold text-white">
-                  {item.title}
-                </h1>
-                <small className="text-[#D21C23] text-[10px] font-semibold sm:text-xs">
-                  {item.date}
-                </small>
-                <p className="text-[12px] sm:text-sm text-gray-300">
-                  {item.description}
-                </p>
-                <div className="right-container-arrow"></div>
-              </div>
-            </motion.div>
+        {/* Left Container 1 */}
+        <motion.div
+          ref={leftContainerRef1}
+          initial={{ opacity: 0, x: isMobile ? 100 : -100 }} // Adjust based on screen size
+          animate={leftInView1 ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 1, delay: 0.1 }} // Initial delay for first item
+          className="containers left-container"
+        >
+          <img src="/record.png" alt="" />
+          <div className="text-box flex flex-col gap-1 sm:gap-3 sm:text-end shadow-lg rounded-lg p-4 hover:shadow-xl transition-all duration-300 transform hover:scale-105">
+            <h1 className="text-base lg:text-lg font-semibold text-white">
+              Frontend Developer at Xcrino
+            </h1>
+            <small className="text-[#D21C23] font-semibold text-[10px] sm:text-xs">
+              2024 - Present
+            </small>
+            <p className="text-[12px] sm:text-sm text-gray-300 text-justify line-clamp-4">
+              Joined Xcrino Business Solutions as a Frontend Developer Intern,
+              to enhancing my skills in React.js, Next.js, and modern web
+              technologies.
+            </p>
+            <div className="left-container-arrow"></div>
           </div>
-        ))}
+        </motion.div>
+
+        {/* Right Container 1 */}
+        <motion.div
+          ref={rightContainerRef1}
+          initial={{ opacity: 0, x: isMobile ? 100 : 100 }} // Adjust based on screen size
+          animate={rightInView1 ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 1, delay: 0.5 }} // Increased delay for the second item
+          className="containers right-container"
+        >
+          <img src="/record.png" alt="" />
+          <div className="text-box flex flex-col gap-3 shadow-lg rounded-lg p-4 hover:shadow-xl transition-all duration-300 transform hover:scale-105">
+            <h1 className="text-base lg:text-lg font-semibold text-white ">
+              MCA (Master's Of Computer Science)
+            </h1>
+            <small className="text-[#D21C23] text-[10px] font-semibold sm:text-xs">
+              2024 - Present
+            </small>
+            <p className="text-[12px] sm:text-sm text-gray-300 text-justify line-clamp-4">
+              Pursuing my MCA from Ganga Institute of Technology and Management,
+              affiliated with Maharishi Dayanand University (MDU).
+            </p>
+            <div className="right-container-arrow"></div>
+          </div>
+        </motion.div>
+
+        {/* Left Container 2 */}
+        <motion.div
+          ref={leftContainerRef2}
+          initial={{ opacity: 0, x: isMobile ? 100 : -100 }} // Adjust based on screen size
+          animate={leftInView2 ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 1, delay: 1.0 }} // Further increased delay
+          className="containers left-container"
+        >
+          <img src="/record.png" alt="" />
+          <div className="text-box flex flex-col gap-1 sm:gap-3 sm:text-end shadow-lg rounded-lg p-4 hover:shadow-xl transition-all duration-300 transform hover:scale-105">
+            <h1 className="text-base lg:text-lg font-semibold text-white">
+              Bca (Bachelor's in Computer Applications)
+            </h1>
+            <small className="text-[#D21C23] font-semibold text-[10px] sm:text-xs">
+              2020 - 2023
+            </small>
+            <p className="text-[12px] sm:text-sm text-gray-300 text-justify line-clamp-4">
+              Graduated from Vash College of Engineering, affiliated with (MDU)
+              Maharishi Dayanand University, with a first division and
+              distinction (approx. 76%).
+            </p>
+            <div className="left-container-arrow"></div>
+          </div>
+        </motion.div>
+
+        {/* Right Container 2 */}
+        <motion.div
+          ref={rightContainerRef2}
+          initial={{ opacity: 0, x: isMobile ? 100 : 100 }} // Adjust based on screen size
+          animate={rightInView2 ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 1, delay: 1.5 }} // Increased delay for the fourth item
+          className="containers right-container"
+        >
+          <img src="/record.png" alt="" />
+          <div className="text-box flex flex-col gap-3 shadow-lg rounded-lg p-4 hover:shadow-xl transition-all duration-300 transform hover:scale-105">
+            <h1 className="text-base lg:text-lg font-semibold text-white">
+              Class 12th
+            </h1>
+            <small className="text-[#D21C23] text-[10px] font-semibold sm:text-xs">
+              2019 - 2020
+            </small>
+            <p className="text-[12px] sm:text-sm text-gray-300 text-justify line-clamp-4">
+              Completed my Class 12 from Bal Bharti School, Bahadurgarh,
+              specializing in the Humanities stream with an impressive score of
+              82%.
+            </p>
+            <div className="right-container-arrow"></div>
+          </div>
+        </motion.div>
       </div>
     </>
   );
