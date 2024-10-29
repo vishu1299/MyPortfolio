8bf90c2b9dbe3b0d5a70d20c72ced7d4324936de
vishu1299 <vishvasgupta129@gmail.com>
2024-10-29 13:02:12 +0530

    footer section responsive

--- src/routes/-components/ContactUs/Components/contact_form.tsx
+++ src/routes/-components/ContactUs/Components/contact_form.tsx
@@ -1,52 +1,120 @@
+import { useForm } from "react-hook-form";
+
 const Contact_form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    alert("Congratulations Your message Has Been Sent");
+    reset();
+  };
   return (
-    <div className="bg-[#171a1e58] p-6 sm:p-10 rounded-xl w-full ">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
-        <div>
-          <label className="block text-white mb-3">Your Name:</label>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="bg-[#171a1e58] p-6 sm:p-10 rounded-xl w-full ">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+          <div>
+            <label className="block text-white mb-3">Your Name:</label>
+            <input
+              placeholder="Name :"
+              className="w-full p-3 focus:outline-none  rounded-md border bg-transparent border-[#c4cfde61] text-gray-300 placeholder-gray-400"
+              {...register("name", {
+                required: "Please Enter Your Name",
+                minLength: {
+                  value: 5,
+                  message: "minimum 5 alphabets are required ",
+                },
+                maxLength: {
+                  value: 15,
+                  message: "maximum 15 alphabets can be used ",
+                },
+              })}
+            />
+            {errors.name?.message && (
+              <p className="text-red-600 pt-2">{String(errors.name.message)}</p>
+            )}
+          </div>
+          <div>
+            <label className="block text-white mb-3">Your Email:</label>
+            <input
+              type="email"
+              placeholder="Email :"
+              className="w-full p-3 rounded-md border focus:outline-none bg-transparent border-[#c4cfde61] text-gray-300 placeholder-gray-400"
+              {...register("Email", {
+                required: "Email Is Required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Invalid Email Eddress",
+                },
+              })}
+            />
+            {errors.Email?.message && (
+              <p className="text-red-600 pt-2">
+                {String(errors.Email.message)}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="mt-8">
+          <label className="block text-white mb-3">Your Question:</label>
           <input
             type="text"
-            placeholder="Name :"
-            className="w-full p-3 rounded-md border bg-transparent border-[#c4cfde61] text-gray-300 placeholder-gray-400"
+            placeholder="Subject :"
+            className="w-full p-3 rounded-md border focus:outline-none bg-transparent border-[#c4cfde61] text-gray-300 placeholder-gray-400"
+            {...register("Question", {
+              required: "Ask Something",
+              minLength: {
+                value: 5,
+                message: "Minimum 5 Alphabets Are required ",
+              },
+              maxLength: {
+                value: 30,
+                message: "Maximum 30 Alphabets Can Be Used ",
+              },
+            })}
           />
+          {errors.Question?.message && (
+            <p className="text-red-600 pt-2">
+              {String(errors.Question.message)}
+            </p>
+          )}
         </div>
-        <div>
-          <label className="block text-white mb-3">Your Email:</label>
-          <input
-            type="email"
-            placeholder="Email :"
-            className="w-full p-3 rounded-md border bg-transparent border-[#c4cfde61] text-gray-300 placeholder-gray-400"
+
+        <div className="mt-8">
+          <label className="block text-white mb-3">Your Comment:</label>
+          <textarea
+            rows={8}
+            placeholder="Message :"
+            className="w-full p-2 rounded-md border focus:outline-none bg-transparent border-[#c4cfde61] text-gray-300 placeholder-gray-400"
+            {...register("Comment", {
+              maxLength: {
+                value: 50,
+                message: "Maximum 50 Alphabets Can Be Used ",
+              },
+            })}
           />
+          {errors.Comment?.message && (
+            <p className="text-red-600 pt-2">
+              {String(errors.Comment.message)}
+            </p>
+          )}
+        </div>
+
+        <div className="mt-8">
+          <button
+            type="submit"
+            className="w-full p-3 rounded-md bg-[#d21c23] text-white font-bold hover:bg-red-700 transition "
+          >
+            SEND MESSAGE
+          </button>
         </div>
       </div>
-
-      <div className="mt-8">
-        <label className="block text-white mb-3">Your Question:</label>
-        <input
-          type="text"
-          placeholder="Subject :"
-          className="w-full p-3 rounded-md border bg-transparent border-[#c4cfde61] text-gray-300 placeholder-gray-400"
-        />
-      </div>
-
-      <div className="mt-8">
-        <label className="block text-white mb-3">Your Comment:</label>
-        <textarea
-          rows={8}
-          placeholder="Message :"
-          className="w-full p-2 rounded-md border bg-transparent border-[#c4cfde61] text-gray-300 placeholder-gray-400"
-        />
-      </div>
-
-      <div className="mt-8">
-        <button
-          disabled
-          className="w-full p-3 rounded-md bg-[#d21c23] text-white font-bold hover:bg-red-700 transition cursor-not-allowed"
-        >
-          SEND MESSAGE
-        </button>
-      </div>
-    </div>
+    </form>
   );
 };
 
--- src/routes/-components/Footer/footer.tsx
+++ src/routes/-components/Footer/footer.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import "../../../style.css";
+import { Link } from "react-scroll";
 
 const Footer: React.FC = () => {
   // Ref for the canvas element
@@ -146,30 +147,61 @@ const Footer: React.FC = () => {
         className="absolute top-0 left-0 w-full h-full z-0"
       ></canvas>
 
-      <div className="relative z-10 grid grid-cols-1 md:grid-cols-3 gap-16 max-w-[90%] mx-auto px-6 py-14">
+      <div className="relative z-10 grid grid-cols-1 sm:grid-cols-2  xl:grid-cols-3 gap-16 sm:gap-8 lg:gap-16 max-w-[90%] mx-auto px-3 py-7 sm:px-6 sm:py-14">
         <div>
-          <h2 className="text-white text-3xl lg:text-6xl font-extrabold mb-4">
+          <h2 className="text-white text-4xl md:text-2xl lg:text-6xl font-extrabold mb-4">
             LET'S TALK
           </h2>
           <button
             type="submit"
-            className="theme-btn max-w-fit mt-5 p-2 text-sm font-bold  xl:text-xs mx-auto"
+            className="theme-btn max-w-fit mt-5 p-2 text-xs md:text-[10px] font-bold  xl:text-xs mx-auto"
           >
-            <div className="mx-auto">CLICK TO CONTACT</div>
+            <div className="mx-auto ">CLICK TO CONTACT</div>
           </button>
         </div>
 
         <div>
           <h3 className="text-white text-2xl font-bold mb-4">Quick Link</h3>
-          <ul className="grid grid-cols-2 sm:grid-cols-4 sm:flex gap-4  mb-4">
-            <li className="hover:text-white text-sm cursor-pointer">RESUME</li>
-            <li className="hover:text-white text-sm cursor-pointer">
-              PORTFOLIO
-            </li>
-            <li className="hover:text-white text-sm cursor-pointer">BLOG</li>
-            <li className="hover:text-white text-sm cursor-pointer">CONTACT</li>
-          </ul>
-          <form className="flex flex-col">
+
+          <div className="grid grid-cols-2 sm:grid-cols-4 sm:flex gap-4  mb-4">
+            <Link
+              to="home"
+              className="cursor-pointer hover:text-white text-sm "
+              smooth={true}
+              duration={500}
+              offset={-100}
+            >
+              HOME
+            </Link>
+            <Link
+              to="skills"
+              className="hover:text-white text-sm cursor-pointer"
+              smooth={true}
+              duration={500}
+              offset={-100}
+            >
+              SKILLS
+            </Link>
+            <Link
+              to="portfolio"
+              className="hover:text-white text-sm cursor-pointer"
+              smooth={true}
+              duration={500}
+              offset={-50}
+            >
+              RESUME
+            </Link>
+            <Link
+              to="contact"
+              className="hover:text-white text-sm cursor-pointer"
+              smooth={true}
+              duration={500}
+              offset={-100}
+            >
+              CONTACT
+            </Link>
+          </div>
+          <form className="flex flex-col pt-3 sm:pt-0">
             <input
               type="email"
               placeholder="Email Address"
@@ -184,12 +216,20 @@ const Footer: React.FC = () => {
           </form>
         </div>
 
-        <div>
-          <h3 className="text-white font-bold text-2xl mb-4 ">Address</h3>
+        <div className="sm:pt-6 xl:pt-0">
+          <h3 className="text-white font-bold text-2xl mb-4 mx-auto">
+            Address
+          </h3>
           <div className="flex flex-col gap-3">
-            <p>8522 Preston Rd. Inglewood, Maine 98380</p>
-            <p>help@gmail.com</p>
-            <p>(907) 555-0101</p>
+            <p>Bahadurgarh,Haryana</p>
+            <p>
+              <span className="font-semibold">Email: </span>{" "}
+              vishvasgupta2002@gmail.com
+            </p>
+            <p>
+              {" "}
+              <span className="font-semibold">Phone-no: </span> (+91) 9416979830
+            </p>
           </div>
         </div>
       </div>
